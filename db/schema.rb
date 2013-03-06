faf5f5650db42e72e2ab5eeec4afc049d5bc34db
Ricardo Cruz <ricardocruz.a89@gmail.com>
2013-03-06 01:32:20 -0600

    added user_id to blogpost column

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130306055746) do
+ActiveRecord::Schema.define(:version => 20130306073131) do
 
   create_table "blogposts", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
 end
