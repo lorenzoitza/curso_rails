1cead72aab26e3ff1afe2c2ab2a9269820390e41
Ricardo Cruz <ricardocruz.a89@gmail.com>
2013-03-10 21:38:27 -0600

    added ajax on the comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @blog_post }
+        format.js
       else
         format.html { render "new" }
       end
